f358240138ead24a2c03b8124782d270e636ce98
mrohith29 <mariyalarohith29@gmail.com>
2023-06-25 10:49:04 +0530

    in progress...

---
+++ numpy/1D _to_2D_convertion.py
@@ -0,0 +1,19 @@
+# First import the numpy module as np.
+import numpy as np
+
+
+a = np.arange(10)
+print(a.shape)
+
+# We can convert from one dimention to two dimention, two dimention to three dimention, two dimention two three dimention.
+a = a[np.newaxis, :]
+print(a.shape)
+print(a)
+
+
+b = np.array([[1,2,3,4]])
+print(b.shape)
+b = np.expand_dims(b, axis=1)
+print(b.shape)
+print(b)
+
