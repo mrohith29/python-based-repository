3f2f95f9b4cf5c1d203290193b284bc0bae1e0c1
mrohith29 <mariyalarohith29@gmail.com>
2023-06-25 08:49:05 +0530

    running..

---
+++ numpy/array.py
@@ -0,0 +1,5 @@
+# Array in numpy are better than lists because :
+# Arrays are much faster than the lists in python,
+# and they store homogeneous values in them,
+# operations like arithmatic operations are made with each element,
+# They store less space than the list.
