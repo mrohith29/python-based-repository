4aff8403f3b673c6ecdf0f985fad01ada0de35a3
mrohith29 <mariyalarohith29@gmail.com>
2023-08-02 22:18:49 +0530

    Learned some basics about pointer usage in python

---
+++ numpy/pointers.py
@@ -0,0 +1,14 @@
+# a = 29                         
+# b = a                         
+# print(b)                         
+# b = 30                         
+# print(b)                         
+# a = [1, 2, 3]
+# b = a                         
+# b.append(10)                         
+# print(a, b)                         
+a = (1, 2, 3)                         
+b = a                         
+print(type(b))                         
+b = list(b)                         
+print(type(b))                         
