cdb603c22fab3540e9143cfabc8f0c13f37fa493
mrohith29 <mariyalarohith29@gmail.com>
2023-08-02 22:33:49 +0530

    updated

--- numpy/pointers.py
+++ numpy/pointers.py
@@ -1,14 +1,22 @@
-# a = 29                         
-# b = a                         
-# print(b)                         
-# b = 30                         
-# print(b)                         
-# a = [1, 2, 3]
-# b = a                         
-# b.append(10)                         
-# print(a, b)                         
-a = (1, 2, 3)                         
+# pointer as a reference veriable.
+'''a = 29                         
 b = a                         
+print(b)                         
+b = 30                         
+print(b)'''                       
+
+# pointer to a list, if we change the value of the reference veriable then the data in the original variable will also change 
+'''a = [1, 2, 3]
+b = a                         
+b.append(10)                         
+print(a, b)                         
+a = (1, 2, 3)                         
+b = a               
 print(type(b))                         
 b = list(b)                         
-print(type(b))                         
+print(type(b))'''
+
+dict = {'a' : 1, 'b' : 2}
+dict['c'] = 3
+print(dict)
+
